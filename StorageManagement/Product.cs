607858dad8ed16a5dc0d5cbffa996763b9e6e4c8
FCryptoFx <ferrancryptofx@gmail.com>
2023-07-28 12:41:24 +0200

    Agregar archivos de proyecto.

---
+++ StorageManagement/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageManagement
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? ProdCode { get; set; }
+        public string? Description { get; set; }
+        public string? Location { get; set; }
+        public string? Storage { get; set; }
+        public int? CalcStore { get; set; }
+
+        string connectionString = "Data Source=LOCALHOST;Initial Catalog=\"Storage Management\";Integrated Security=True";
+    }
+}
